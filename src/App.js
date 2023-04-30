08c66aaecfe8931c4725402bcead726e5f0c713a
DIGIPlus <admin@digiplus.cf>
2023-04-30 22:31:21 +0400

    SIDE EFFECT

--- src/App.js
+++ src/App.js
@@ -1,14 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React from "react"
+import React, { useEffect } from "react"
 
 export default function App() {
-    const [data, setData] = React.useState({})
+//     const [starWarsData, setStarWarsData] = React.useState({})
+
+//     // 1. GET the data (fetch)
+//     // 2. Save the data to state
+    
+//     // loop infinit console.log("Component rendered")
+
+//     // fetch("https://swapi.dev/api/people/1")
+//     //     .then(res => res.json())
+//     //     //.then(data => setStarWarsData(data))
+//     //     .then(data => console.log(data))
+ 
+ 
+//     // side effects
+ 
+//     React.useEffect( function(){
+//       fetch("https://swapi.dev/api/people/1")
+//           .then(res => res.json())
+//           .then(data => setStarWarsData(data))
+//           .then(data => console.log(data))
+//     })     
+
+//     return (
+//         <div>
+//             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+//         </div>
+//     )
+// }
+
+const [starWarsData, setStarWarsData] = React.useState({})
+const [count, setCount] = React.useState(0)
+
+    // 1. GET the data (fetch)
+    // 2. Save the data to state
+    
+    // loop infinit console.log("Component rendered")
+
+    // fetch("https://swapi.dev/api/people/1")
+    //     .then(res => res.json())
+    //     //.then(data => setStarWarsData(data))
+    //     .then(data => console.log(data))
+ 
+ 
+    // side effects
+ 
+    React.useEffect( function(){
+      fetch("https://swapi.dev/api/people/1")
+          .then(res => res.json())
+          .then(data => setStarWarsData(data))
+          .then(data => console.log(data))
+    }, [count])     
 
     return (
         <div>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
         </div>
     )
 }
